aca56495b0f4da87adec10497d26ca065782cb8e
antimo123456789 <55553@dmjx.dk>
2025-05-13 15:10:03 +0200

    Cat_animation

---
+++ Go-Again/Assets/Antimo's private mappe/Kill_yourself.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Antimo_s_private_mappe
+{
+    public class KillYourself : MonoBehaviour
+    {
+        private Animator _animator;
+
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) // or any key you like
+            {
+                _animator.SetTrigger("PlayAnim"); // Use the name of your animation clip
+            }
+        }
+    }
+}
