6b9a1f8fce1a6f52b2fd7a91893863411bae708e
Baldrian Sector <baldriansector@gmail.com>
2025-05-13 10:42:48 +0200

    Respawner and import particle effects

---
+++ Go-Again/Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Spawner Settings")]
+    public GameObject player; // Reference to the player object
+
+    [Header("Movement Settings")]
+    public Vector3 positionOffset = new Vector3(0, 1.5f, 0); // Optional offset (e.g. for height)
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M) && player != null)
+        {
+            transform.position = player.transform.position + positionOffset;
+            Debug.Log("Spawner moved to: " + transform.position);
+        }
+    }
+}
